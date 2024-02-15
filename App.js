490415751a312e99c8695644b5aab45d53dca2e8
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-02-15 17:11:31 -0300

    structure

--- App.js
+++ App.js
@@ -4,9 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.boasVindas}>oi dfhdf</Text>
-      <Text style={styles.estilo2}>oi dfhdf0</Text>
+     
+     <View style={styles.header}>
+          <View style={styles.avatar}></View>
+          <Text style={styles.boasVindas}>Luis Eduardo</Text>
+      </View>
+
+
+      <View style={styles.body}>
+        <Text style={styles.boasVindas}>Bem vindo</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.boasVindas}>©Copyryght 2024</Text>
+      </View>
+
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -16,16 +31,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingTop: 40,
   },
-  boasVindas: {
-    color: '#024589',
-    backgroundColor: '#dddddd',
-    fontSize: 30
+  boasVindas:{
+    color: '#fff',
+    fontSize: 25
   },
-  estilo2: {
-    color: '#459812',
-    backgroundColor: '#aaaaaa',
-    fontSize: 60
+  header: {
+    flex:1,
+    width: '100%',
+    color: '#564655',
+    backgroundColor: '##E4E5E7',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+
+  body: {
+    flex:3,
+    color: 'red',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#525252',
+    fontSize:  90,
+  },
+
+  footer: {
+    flex:1,
+    color: 'red',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0B0C0E',
+    fontSize: 30,
+  },
+  avatar:{
+    width: 50,
+    height: 50,
+    backgroundColor: 'blue',
+    borderRadius: 50,
+    marginHorizontal: 10 
   },
 });
