2d7708ccf2b2f41e3808cc14dd19751e9a2541a3
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-07 16:48:31 -0300

    add flat list

--- components/Body.js
+++ components/Body.js
@@ -1,76 +1,129 @@
-import { ScrollView, StyleSheet, Text, View} from 'react-native';
-import { Image } from 'expo-image';
-import H1 from './ui/H1'
-import CardUser from './CardUser';
+import { ScrollView, StyleSheet, Text, View, FlatList } from "react-native";
+import { Image } from "expo-image";
+import H1 from "./ui/H1";
+import CardUser from "./CardUser";
+
+const users = [
+  {
+    id: 1,
+    name: "Luis",
+    email: "luis@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+
+  {
+    id: 2,
+    name: "Maria",
+    email: "maria@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+
+  {
+    id: 3,
+    name: "Carlos",
+    email: "carlos@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+  {
+    id: 4,
+    name: "Fernanda",
+    email: "carlos@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+  {
+    id: 5,
+    name: "Anderson",
+    email: "carlos@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+
+  {
+    id: 6,
+    name: "Anderson",
+    email: "carlos@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+
+  {
+    id: 7,
+    name: "Anderson",
+    email: "carlos@gdfng.vfio",
+    avatar: "https://avatars.githubusercontent.com/u/133153563?v=4",
+  },
+];
 
 const Body = () => {
   return (
     <View style={styles.body}>
-       <H1>Usuários</H1>
-       <CardUser 
-       avatar= "https://avatars.githubusercontent.com/u/133153563?v=4" 
-       name= "Luis"
-       email="luis0202.eduardo@gmail.com"
-       />
-
-       <CardUser 
-       avatar= "https://avatars.githubusercontent.com/u/133153563?v=4" 
-       name= "Maria"
-       email="maria.eduardo@gmail.com"
-       />
-
-       <CardUser 
-       avatar= "https://avatars.githubusercontent.com/u/133153563?v=4" 
-       name= "Carlos"
-       email="carlos.eduardo@gmail.com"
-       />
-  </View> 
-   )
-} 
+      <H1 style={styles.usuario}>Usuários</H1>
+      <View style={styles.listUser}>
+        <FlatList
+          data={users}
+          renderItem={({ item }) => <CardUser user={item} />}
+          keyExtractor={(user) => user.id}
+          horizontal={true}
+        />
+      </View>
+      
+      <H1 style={styles.usuario}>Usuários</H1>
+      <View style={styles.listUserr}>
+        <FlatList
+          data={users}
+          renderItem={({ item }) => <CardUser user={item} />}
+          keyExtractor={(user) => user.id}
+        />
+      </View>
+    </View>
+    
+  );
+};
 
 const styles = StyleSheet.create({
-    body: {
-        flex:3,
-        color: 'red',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        // backgroundColor: '#525252',
-        fontSize:  90,
-      },
-      Image:{
-        marginHorizontal: 17,
-        marginBottom: 10 ,
-        flex: 1,
-        width: "90%",
-        height: "60%",
-        borderRadius: 7, 
-      },  
-      boasVindas:{
-        fontSize: 25,
-        color: "white",
-      },
-      card:{
-        marginTop: 40,
-        backgroundColor: "#9AA0A6",        
-        width: 370,
-        height: 300,
-        borderRadius: 7, 
-      },
-      titulo:{
-        marginTop: 15,
-        marginLeft: 10,
-        fontSize: 20,
-        color: "white",
-      },
-      txt:{
-        marginTop: 15,
-        marginLeft: 20,
-        fontSize: 15,
-        color: "white",
-      }
-    
-})
+  body: {
+    flex: 3,
+    color: "red",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 90,
+  },
+  Image: {
+    marginHorizontal: 17,
+    marginBottom: 10,
+    flex: 1,
+    width: "90%",
+    height: "60%",
+    borderRadius: 7,
+  },
+  boasVindas: {
+    fontSize: 25,
+    color: "white",
+  },
+  card: {
+    marginTop: 40,
+    backgroundColor: "#9AA0A6",
+    width: 370,
+    height: 300,
+    borderRadius: 7,
+  },
+  titulo: {
+    marginTop: 15,
+    marginLeft: 10,
+    fontSize: 20,
+    color: "white",
+  },
+  txt: {
+    marginTop: 15,
+    marginLeft: 20,
+    fontSize: 15,
+    color: "white",
+  },
+  listUser: {
+    height: 120,
+  },
+  listUserr: {
+    height: 350,
+  },
+});
 
-
-export default Body
+export default Body;
