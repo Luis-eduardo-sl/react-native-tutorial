ce306638180578ab7d73896a0c906b8b5e3e6d19
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-15 17:39:31 -0300

    delete background Image

--- App.js
+++ App.js
@@ -3,19 +3,13 @@ import { StyleSheet, View, ImageBackground} from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Body from './components/Body';
-// import { ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground 
-      resizeMode="cover" 
-      source={require('./assets/images/bg-mobile.jpg')}
-      style={styles.bg}>
         <Header />
         <Body />
         <Footer />
-      </ImageBackground>
       <StatusBar style="light" />
 
     </View>
@@ -25,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
+    backgroundColor: '#525252',
     alignItems: 'center',
     justifyContent: 'center',
   },
