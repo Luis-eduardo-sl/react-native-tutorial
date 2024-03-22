808a1b681ee8720b3ea6f867c1bcd2c51ac71627
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-22 15:01:05 -0300

    change name

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Editar from './screens/Editar'
-import listUser from './screens/ListUser';
+import ListUser from './screens/ListUser';
 import Cadastrar from './screens/Cadastrar';
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Principal" component={listUser} />
+        <Stack.Screen name="Principal" component={ListUser} />
         <Stack.Screen name="Cadastrar" component={Cadastrar} />
         <Stack.Screen name="Editar" component={Editar} />
 
